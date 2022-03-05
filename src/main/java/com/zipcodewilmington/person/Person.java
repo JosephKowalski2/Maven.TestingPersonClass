eca3c446e9377391d03bfbf13a730e8df6a20455
Joseph Kowalski <jkjr25@gmail.com>
2022-03-05 13:19:40 -0500

    Added set and get methods

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -31,6 +31,16 @@ public class Person {
         this.age = age;
     }
 
+    public Person(String name, int age, String dob, String address, int phonenumber, int numofpets, String email) {
+        this.name = name;
+        this.age = age;
+        this.dob = dob;
+        this.address = address;
+        this.phonenumber = phonenumber;
+        this.numofpets = numofpets;
+        this.email = email;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
@@ -39,11 +49,51 @@ public class Person {
         this.age = age;
     }
 
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPhonenumber(int phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public void setNumofpets(int numofpets) {
+        this.numofpets = numofpets;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getName() {
         return this.name;
     }
 
     public Integer getAge() {
         return this.age;
+    }
+
+    public String getDob() {
+        return this.dob;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public Integer getPhonenumber() {
+        return this.phonenumber;
+    }
+
+    public Integer getNumofpets() {
+        return this.numofpets;
+    }
+
+    public String getEmail() {
+        return this.email;
     }
 }
